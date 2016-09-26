e11418f26b633adfb576eaaf8c4142b508b2e5cc
Naveed Zaheer <Naveed Zaheer>
2016-09-26 14:27:47 -0500

    First-check-in

---
+++ api/note.js
@@ -0,0 +1,85 @@
+var azure = require('azure-storage');
+var uuid = require('node-uuid');
+var entityGen = azure.TableUtilities.entityGenerator
+var tableService = azure.createTableService();
+var userName = "nzaheer";
+
+tableService.createTableIfNotExists("notes", function(error, result, response) {
+  if (!error) {
+    console.info(result);
+  }
+  else{
+    console.warn(error);      
+  }
+});
+
+var api = {
+    
+    post: function (req, res, next) {
+        var date = { currentTime: Date.now() };
+        var uuid1 = uuid.v1();
+        var bodyJson = req.body;
+
+        var entity = {
+            PartitionKey: entityGen.String(userName),
+            RowKey: entityGen.String(uuid1),
+            dateWritten: entityGen.DateTime(new Date()),
+            userId: entityGen.String(userName),
+            noteData: entityGen.String(req.body.notes)
+        };
+        console.info("Creating Entity");
+        tableService.insertEntity("notes", entity, function(error, result, response) {
+        if (error) {
+            console.warn(error);
+            res.status(500).type('application/json').send(error);
+        }
+        else{
+            console.info(result);
+            console.info(result);
+            res.status(200).type('application/json').send(result);
+        }
+        });
+    },
+
+    get: function (req, res, next) {
+        //console.info(JSON.stringify(req.azureMobile.user));
+        
+        var query = new azure.TableQuery()
+            .top(100)
+            .where("PartitionKey eq ?", userName);
+
+        console.info("Getting Entities");
+        tableService.queryEntities('notes', query, null, function(error, result, response) {
+        if (error) {
+            console.warn(error);
+            res.status(500).type('application/json').send(error);
+        }
+        else{
+            console.info(result);
+            console.info(response);
+            res.status(200).type('application/json').send(result);
+        }
+        });
+    },
+
+    delete: function (req, res, next) {
+        var entity = {
+            PartitionKey: entityGen.String(userName),
+            RowKey: entityGen.String(req.body.key)
+        };
+
+        console.info("Deleting Entities");
+        tableService.deleteEntity('notes', entity, function(error, response) {
+        if (error) {
+            console.warn(error);
+            res.status(500).type('application/json').send(error);
+        }
+        else{
+            console.info(response);
+            res.status(200).type('application/json').send(result);
+        }
+        });
+    }
+};
+
+module.exports = api;
---
+++ tables/task.js
@@ -0,0 +1,63 @@
+﻿// ----------------------------------------------------------------------------
+// Copyright (c) 2015 Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+/*
+** Sample Table Definition - this supports the Azure Mobile Apps
+** TodoItem product with authentication and offline sync
+*/
+var azureMobileApps = require('azure-mobile-apps');
+
+// Create a new table definition
+var table = azureMobileApps.table();
+
+//table.access = 'authenticated';
+
+// Table Definition 
+table.name = 'task';
+table.schema = 'dbo';
+table.databaseTableName = 'task';
+table.maxTop = 1000;
+table.dynamicSchema = false;
+table.columns = {
+        userid: 'string',
+        text: 'string',
+    category: 'string',
+        due: 'datetime',
+        alert: 'number',
+        complete: 'boolean'
+};
+
+// Configure specific code when the client does a request
+// READ - only return records belonging to the authenticated user
+table.read(function (context) {
+    //console.info("user name -", JSON.stringify(context.user));
+    //context.query.where({ userId: context.user.id });
+    return context.execute();
+});
+
+// CREATE - add or overwrite the userId based on the authenticated user
+table.insert(function (context) {
+    //console.info("user name -", JSON.stringify(context.user));
+    //context.item.userId = context.user.id;
+    return context.execute();
+});
+
+// UPDATE - for this scenario, we don't need to do anything - this is
+// the default version
+table.update(function (context) {
+    //console.info("user name -", JSON.stringify(context.user));
+    return context.execute();
+});
+
+// DELETE - for this scenario, we don't need to do anything - this is
+// the default version
+table.delete(function (context) {
+    //console.info("user name -", JSON.stringify(context.user));
+    return context.execute();
+});
+
+// Finally, export the table to the Azure Mobile Apps SDK - it can be
+// read using the azureMobileApps.tables.import(path) method
+
+module.exports = table;
